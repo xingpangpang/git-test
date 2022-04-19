c020a985f53d7169a5d5a81c785daa70732d7452
Augenstern <Augenstern@qqcom>
2022-04-19 14:54:17 +0800

    hot-fix commit

--- src/main/java/cn/tedu/git/GitTest.java
+++ src/main/java/cn/tedu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("no!no!no!");
+        System.out.println("hot-fix test!");
     }
 
 }
